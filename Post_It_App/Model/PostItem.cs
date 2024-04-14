1763c50aff247cffdf52213c48ae1665b785af34
Murilo Pistore <91626828+TheBud4@users.noreply.github.com>
2024-04-14 19:18:05 -0300

    Nome Atualizado

---
+++ Post_It_App/Model/PostItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Post_It_App.Model;
+internal class PostItem {
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public PostItem(string title, string description) {
+        Title = title;
+        Description = description;
+        Id = new Random().Next(1000, 9999);
+    }
+}
---
+++ Post_It_App/Model/PostManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Post_It_App.Model {
+    internal class PostManager {
+        private readonly List<PostItem> _posts;
+
+        public PostManager() {
+            _posts = new List<PostItem>();
+        }
+
+        public void AddPost(PostItem post) {
+            _posts.Add(post);
+        }
+
+        public void UpdatePost(int id, string title, string description) {
+            var post = _posts.FirstOrDefault(p => p.Id == id);
+            if (post != null) {
+                post.Title = title;
+                post.Description = description;
+            }
+        }
+
+        public void DeletePost(int id) {
+            var post = _posts.FirstOrDefault(p => p.Id == id);
+            if (post != null) {
+                _posts.Remove(post);
+            }
+        }
+
+        public List<PostItem> GetAllPosts() {
+            return _posts.ToList();
+        }
+
+        public PostItem GetPostById(int id) {
+            return _posts.FirstOrDefault(p => p.Id == id);
+        }
+    }
+}
